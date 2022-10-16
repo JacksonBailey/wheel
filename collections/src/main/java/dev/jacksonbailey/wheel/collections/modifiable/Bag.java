5b07acf773fecf109b1853a5143832adc4a7f229
Jackson Bailey <jackson.robert.bailey@gmail.com>
2022-10-16 17:22:09 -0400

    Add some default methods to Bag

--- collections/src/main/java/dev/jacksonbailey/wheel/collections/modifiable/Bag.java
+++ collections/src/main/java/dev/jacksonbailey/wheel/collections/modifiable/Bag.java
@@ -4,18 +4,27 @@ import static java.util.Objects.requireNonNull;
 
 import dev.jacksonbailey.wheel.collections.Walkable;
 import dev.jacksonbailey.wheel.collections.Walker;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
 
+/**
+ * In general these methods return true if the bag is modified.
+ */
 public interface Bag<E> extends Walkable<E> {
 
   boolean add(E e);
-  boolean addAll(Bag<? extends E> c);
+  default boolean addAll(Bag<? extends E> b) {
+    return applyToAll(this::add, b);
+  }
 
   boolean remove(Object o);
-  boolean removeAll(Bag<?> c);
+  default boolean removeAll(Bag<?> b) {
+    return applyToAll(this::remove, b);
+  }
 
   // TODO Is there a way to do this without optional operation?
-  // TODO Should modifying a Bag while walking be undefined?
+  // TODO Should modifying a Bag while walking be undefined like JCL?
   default boolean removeIf(Predicate<? super E> filter) throws UnsupportedOperationException {
     requireNonNull(filter);
     var modified = false;
@@ -29,12 +38,21 @@ public interface Bag<E> extends Walkable<E> {
     return modified;
   }
 
-  boolean retainAll(Bag<?> c);
+  default boolean retainAll(Bag<?> b) {
+    return applyToAll(e -> removeIf(Predicate.not(b::contains)), b);
+  }
   void clear();
 
   // Looking
-  boolean contains(Object o);
-  boolean containsAll(Bag<?> c);
+  default boolean contains(Object o) {
+    for (E e: this) {
+      if (Objects.equals(o, e)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  boolean containsAll(Bag<?> b);
 
   // Sizing
   default boolean isEmpty() {
@@ -48,5 +66,12 @@ public interface Bag<E> extends Walkable<E> {
 
   // Walking
   Walker<E> walker();
+
+  private static <B> boolean applyToAll(Predicate<? super B> p, Bag<B> b) {
+    // rollingOr returns true if any p.test(e) is true
+    var rollingOr = new AtomicBoolean(false);
+    b.forEach(e -> rollingOr.compareAndSet(false, p.test(e)));
+    return rollingOr.get();
+  }
 
 }
