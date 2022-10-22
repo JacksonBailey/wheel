ddac624716e4506ec118e2563ad20803e802f73a
Jackson Bailey <jackson.robert.bailey@gmail.com>
2022-10-21 20:46:22 -0400

    Organize buildSrc plugins better

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -1,25 +1,8 @@
 plugins {
-    id("dev.jacksonbailey.wheel.base-common-conventions")
-    `java-platform`
-}
-
-javaPlatform {
-    allowDependencies()
+    id("dev.jacksonbailey.wheel.java-platform-conventions")
 }
 
 dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:2.7.4"))
     api("org.jetbrains:annotations:23.0.0")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenPlatform") {
-            from(components["javaPlatform"])
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["mavenPlatform"])
 }
