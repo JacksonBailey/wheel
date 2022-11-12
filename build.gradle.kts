c6ceb53c80fb911a34788834da0aa47b62d7b894
Jackson Bailey <jackson.robert.bailey@gmail.com>
2022-11-12 15:12:20 -0500

    Add plugin to check which dependenices can update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
-    id("com.dorongold.task-tree") version "2.1.0"
+    id("com.dorongold.task-tree") version "2.1.0" // https://github.com/dorongold/gradle-task-tree#usage
+    id("com.github.ben-manes.versions") version "0.44.0" // https://github.com/ben-manes/gradle-versions-plugin#dependencyupdates
 }
 
 allprojects {
     group = "dev.jacksonbailey.wheel"
+}
+
+tasks.withType(DependencyUpdatesTask::class) {
+    revision = "release"
 }
