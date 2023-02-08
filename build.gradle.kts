744778c56e505d47e4f23d4fe241003a1d498b87
Jackson Bailey <jackson.robert.bailey@gmail.com>
2023-02-08 15:27:42 -0500

    Update dependencies
    Also add links to easily find later versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.dorongold.task-tree") version "2.1.0" // https://github.com/dorongold/gradle-task-tree#usage
-    id("com.github.ben-manes.versions") version "0.44.0" // https://github.com/ben-manes/gradle-versions-plugin#dependencyupdates
-    // Below is commented out due to this https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin/issues/798#issuecomment-1312794063
-    // id("com.autonomousapps.dependency-analysis") version "1.13.1" // https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin#add-to-your-project-and-use
+    id("com.dorongold.task-tree") version "2.1.1" // https://github.com/dorongold/gradle-task-tree#usage
+    id("com.github.ben-manes.versions") version "0.45.0" // https://github.com/ben-manes/gradle-versions-plugin#dependencyupdates
+    id("com.autonomousapps.dependency-analysis") version "1.19.0" // https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin#add-to-your-project-and-use
 }
 
 allprojects {
