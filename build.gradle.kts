6dcf7b41cf6a5f3d3e22974ff9862ffde8d096a2
Jackson Bailey <jackson.robert.bailey@gmail.com>
2023-02-22 18:28:40 -0500

    Bump plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     id("com.dorongold.task-tree") version "2.1.1" // https://github.com/dorongold/gradle-task-tree#usage
-    id("com.github.ben-manes.versions") version "0.45.0" // https://github.com/ben-manes/gradle-versions-plugin#dependencyupdates
+    id("com.github.ben-manes.versions") version "0.46.0" // https://github.com/ben-manes/gradle-versions-plugin#dependencyupdates
     id("com.autonomousapps.dependency-analysis") version "1.19.0" // https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin#add-to-your-project-and-use
 }
 
