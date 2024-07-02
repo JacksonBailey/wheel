3c34727693e058fe7eeb69a98ee63228812d3d78
Jackson Bailey <jackson.robert.bailey@gmail.com>
2024-07-01 20:15:57 -0400

    Simplify Channel to ChannelDefinition mapping

--- terra/src/main/java/dev/jacksonbailey/wheel/terra/model/ChannelDefinition.java
+++ terra/src/main/java/dev/jacksonbailey/wheel/terra/model/ChannelDefinition.java
@@ -1,7 +1,16 @@
 package dev.jacksonbailey.wheel.terra.model;
 
 import java.util.List;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.MediaChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.StageChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import org.jetbrains.annotations.NotNull;
 
 public interface ChannelDefinition {
 
@@ -12,5 +21,21 @@ public interface ChannelDefinition {
   Integer position();
 
   List<Overwrite> permissionOverwrites();
+
+  @NotNull
+  static ChannelDefinition from(@NotNull Channel channel) {
+    return switch (channel) {
+      case TextChannel textChannel -> TextChannelDefinition.from(textChannel);
+      case VoiceChannel voiceChannel -> VoiceChannelDefinition.from(voiceChannel);
+      case Category category -> CategoryDefinition.from(category);
+      case NewsChannel newsChannel -> AnnouncementChannelDefinition.from(newsChannel);
+      case StageChannel stageChannel -> StageChannelDefinition.from(stageChannel);
+      case ForumChannel forumChannel -> ForumChannelDefinition.from(forumChannel);
+      case MediaChannel mediaChannel -> MediaChannelDefinition.from(mediaChannel);
+      default -> throw new IllegalArgumentException(
+          "Unexpected type: " + channel.getClass() + ", " + channel
+      );
+    };
+  }
 
 }
--- terra/src/main/java/dev/jacksonbailey/wheel/terra/service/GetChannelsCommand.java
+++ terra/src/main/java/dev/jacksonbailey/wheel/terra/service/GetChannelsCommand.java
@@ -1,23 +1,8 @@
 package dev.jacksonbailey.wheel.terra.service;
 
-import dev.jacksonbailey.wheel.terra.model.AnnouncementChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.CategoryDefinition;
 import dev.jacksonbailey.wheel.terra.model.ChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.ForumChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.MediaChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.StageChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.TextChannelDefinition;
-import dev.jacksonbailey.wheel.terra.model.VoiceChannelDefinition;
-import java.util.ArrayList;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.channel.concrete.Category;
-import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.MediaChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.StageChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -57,76 +42,9 @@ public class GetChannelsCommand extends AbstractCommand {
     var sourceGuild = event.getGuild();
     assert sourceGuild != null; // TODO Improve this??
 
-    // TODO At some point this should be in a utility class.
-    var channels = sourceGuild.getChannels();
-    var channelDefinitions = new ArrayList<ChannelDefinition>();
-    for (var channel : channels) {
-      var channelType = channel.getType();
-      switch (channelType) {
-        case TEXT -> {
-          if (channel instanceof TextChannel textChannel) {
-            channelDefinitions.add(TextChannelDefinition.from(textChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case PRIVATE -> {
-          log.warn("Got a channel of type PRIVATE which should not be in guilds: {}", channel);
-        }
-        case VOICE -> {
-          if (channel instanceof VoiceChannel voiceChannel) {
-            channelDefinitions.add(VoiceChannelDefinition.from(voiceChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case GROUP -> {
-          log.warn("Got a channel of type GROUP which should not exist: {}", channel);
-        }
-        case CATEGORY -> {
-          if (channel instanceof Category category) {
-            channelDefinitions.add(CategoryDefinition.from(category));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case NEWS -> {
-          if (channel instanceof NewsChannel newsChannel) {
-            channelDefinitions.add(AnnouncementChannelDefinition.from(newsChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case STAGE -> {
-          if (channel instanceof StageChannel stageChannel) {
-            channelDefinitions.add(StageChannelDefinition.from(stageChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case GUILD_NEWS_THREAD, GUILD_PUBLIC_THREAD, GUILD_PRIVATE_THREAD -> {
-          log.trace("Got a channel of {} type, but Terra does not deal with threads. {}",
-              channel.getType(), channel);
-        }
-        case FORUM -> {
-          if (channel instanceof ForumChannel forumChannel) {
-            channelDefinitions.add(ForumChannelDefinition.from(forumChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case MEDIA -> {
-          if (channel instanceof MediaChannel mediaChannel) {
-            channelDefinitions.add(MediaChannelDefinition.from(mediaChannel));
-          } else {
-            throw new IllegalStateException();
-          }
-        }
-        case UNKNOWN -> {
-          log.warn("Got a channel of UNKNOWN type: {}", channel);
-        }
-      }
-    }
+    var channelDefinitions = sourceGuild.getChannels().stream()
+                                        .map(ChannelDefinition::from)
+                                        .toList();
 
     var builder = new MessageCreateBuilder();
     return builder.addContent(channelDefinitions.toString()).build();
